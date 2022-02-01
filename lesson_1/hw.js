c6520d64bde78e1013564fc27770a94b99b05227
GrayHead <ss.zhuravlov@gmail.com>
2022-02-01 22:10:13 +0200

    add

---
+++ lesson_1/hw.js
@@ -0,0 +1,52 @@
+let hello = 'hello';
+let title = 'owu';
+let domen = 'com';
+let contr = 'ua';
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let bool1 = true;
+let bool2 = false;
+console.log(hello)
+console.log(title)
+console.log(domen)
+console.log(contr)
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+console.log(number5)
+console.log(number6)
+console.log(number7)
+console.log(bool1)
+console.log(bool2)
+alert(hello)
+alert(title)
+alert(domen)
+alert(contr)
+alert(number1)
+alert(number2)
+alert(number3)
+alert(number4)
+alert(number5)
+alert(number6)
+alert(number7)
+alert(bool1)
+alert(bool2)
+document.write(hello)
+document.write(title)
+document.write(domen)
+document.write(contr)
+document.write(number1)
+document.write(number2)
+document.write(number3)
+document.write(number4)
+document.write(number5)
+document.write(number6)
+document.write(number7)
+document.write(bool1)
+document.write(bool2)
