2152e6089e6fc9e68fec09bcb5ac1a9a661e5a70
GrayHead <ss.zhuravlov@gmail.com>
2022-02-13 22:58:56 +0200

    add lesson_6

--- lesson_6/hw.js
+++ lesson_6/hw.js
@@ -36,14 +36,27 @@
 
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
-
+// let str = ' dirty string   ';
+// document.write(str.substring(0,));
+// document.write(str.substring(13,16));
+///////////////////
+// let str = ' dirty string   ';
+// let clean = str.trim();
+// document.write(clean);
 
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Каждый охотник желает знать';
 // let arr = stringToarray(str);
 // document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
-//
-//
+
+let stringToarray =(str)=>{
+    return  str.split(` `);
+}
+    let str = 'Каждый охотник желает знать';
+let arr = stringToarray(str);
+document.writeln(arr);
+
+
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
 //     let str = 'Каждый охотник желает знать';
 // document.writeln(delete_characters(str, 7)); // Каждый
