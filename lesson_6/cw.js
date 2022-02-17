5ab4406ebe3294447f4eeadc82f9c24cdcd8a6c7
GrayHead <ss.zhuravlov@gmail.com>
2022-02-17 22:25:21 +0200

    add lesson_6

--- lesson_6/cw.js
+++ lesson_6/cw.js
@@ -1,1 +1,64 @@
-1
+//
+//
+// - Дано список імен.
+//     let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// let n1 = 'Harry Potter'
+// let n2 = 'Ron Whisley'
+// let n3 = 'Hermione Granger'
+
+let n1 = 'Harry..Potter'
+let n2 = 'Ron---Whisley'
+let n3 = 'Hermione__Granger'
+//
+// let validator = (str) => {
+//      str
+//         .replaceAll('.',' ' )
+//         .replaceAll('.',' ' )
+// return str;
+// }
+//
+// document.write(validator(n1))
+// document.write(validator(n2))
+// document.write(validator(n3))
+
+
+let name = (string, symbol) => {
+    let newArr = [];
+    if (string.includes(symbol)) {
+        let a = string.split(symbol)
+        a.forEach((item) => {
+            if (item) newArr.push(item);
+        })
+        document.write(newArr.join(' '))
+    }
+}
+name(n1, '.');
+name(n2, '-');
+name(n3, '_');
+
+
+
+//     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+// - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums('ascending') // [3,11,21]
+// sortNums('descending') // [21,11,3]
+//
+//
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
--- lesson_6/hw.js
+++ lesson_6/hw.js
@@ -49,24 +49,49 @@
 // let arr = stringToarray(str);
 // document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
 
-let stringToarray =(str)=>{
-    return  str.split(` `);
-}
-    let str = 'Каждый охотник желает знать';
-let arr = stringToarray(str);
-document.writeln(arr);
+// let stringToarray =(str)=>{
+//     return  str.split(` `);
+// }
+//     let str = 'Каждый охотник желает знать';
+// let arr = stringToarray(str);
+// document.writeln(arr);
+// console.log(arr);
 
 
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
 //     let str = 'Каждый охотник желает знать';
 // document.writeln(delete_characters(str, 7)); // Каждый
-//
+
+// let str = 'Каждый охотник желает знать';
+// let delete_characters = (str, index) => {
+//     return str.substr(0, index);
+// }
+// document.writeln(delete_characters(str, 7));
+
 // - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
 //     let str = "HTML JavaScript PHP";
 // document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
-//
+
+// let str = "HTML JavaScript PHP";
+// let insert_dash = (str) => {
+//     let strUpperSplit = str.split(' ').join('-').toUpperCase();
+//     return strUpperSplit;
+// }
+// document.write(insert_dash(str))
+
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
-//
-//
-//
+
+// let upperFirst=(str)=>{
+//     if(!str) return str;
+//     return str[0].toUpperCase()+str.slice(1);
+// }
+// document.write(upperFirst(`hello my friend <br>`));
+// document.write(upperFirst('fact'));
+
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+
+// let capitalize = (str) => {
+//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+// }
+//
+// document.write(capitalize('приїхали додому'))
