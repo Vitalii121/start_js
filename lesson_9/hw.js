e040234d68e76a365b46282a8b96faac362536a2
GrayHead <ss.zhuravlov@gmail.com>
2022-02-22 22:14:23 +0200

    add lesson_9

---
+++ lesson_9/hw.js
@@ -0,0 +1,53 @@
+// Все робити за допомоги js.
+// - створити блок,
+
+let block =document.createElement('div');
+
+//     - додати йому класи wrap, collapse, alpha, beta
+block.classList.add('wrap')
+block.classList.add('collapse')
+block.classList.add('alpha')
+block.classList.add('beta')
+
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+
+block.style.fontSize='20px'
+block.style.background='black'
+block.style.color='blue'
+
+// - додати цей блок в body.
+
+document.body.append(block);
+
+// - клонувати його повністю, та додати клон в body.
+
+
+
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+//
+// // - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+//
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
